f4f7bd462e5623a56547a5e47e4bb5d327fc8527
Itsusinn <itsusinn@meowcat.org>
2022-08-14 00:23:56 +0800

    feat(tls): use certs path from configuration file

--- src/tls.rs
+++ src/tls.rs
@@ -1,21 +1,30 @@
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 
 use color_eyre::eyre::Result;
 
-pub fn read_certs_from_file() -> Result<(Vec<rustls::Certificate>, rustls::PrivateKey)> {
-  let mut cert_chain_reader = BufReader::new(std::fs::File::open("./res/server-cert.pem")?);
+use crate::config::CONFIG;
+
+pub async fn read_certs_from_file() -> Result<(Vec<rustls::Certificate>, rustls::PrivateKey)> {
+  let cert_file = std::fs::File::open(CONFIG.tls.cert.as_str())?;
+  let mut cert_chain_reader = BufReader::new(cert_file);
   let certs = rustls_pemfile::certs(&mut cert_chain_reader)?
     .into_iter()
     .map(rustls::Certificate)
     .collect::<Vec<_>>();
   assert!(!certs.is_empty());
-
-  let mut key_reader = BufReader::new(std::fs::File::open("./res/server-key.pem")?);
-  // if the file starts with "BEGIN RSA PRIVATE KEY"
-  // let mut keys = rustls_pemfile::rsa_private_keys(&mut key_reader)?;
-  // if the file starts with "BEGIN PRIVATE KEY"
-  let mut keys = rustls_pemfile::pkcs8_private_keys(&mut key_reader)?;
-  // assert!(!keys.is_empty());
+  let key_file = std::fs::File::open(CONFIG.tls.key.as_str())?;
+  let mut key_reader = BufReader::new(key_file);
+  let mut head = String::new();
+  key_reader.read_line(&mut head)?;
+  let key_file = std::fs::File::open(CONFIG.tls.key.as_str())?;
+  let mut key_reader = BufReader::new(key_file);
+  let mut keys = if head.contains("BEGIN RSA PRIVATE KEY") {
+    rustls_pemfile::rsa_private_keys(&mut key_reader)?
+  } else if head.contains("BEGIN PRIVATE KEY") {
+    rustls_pemfile::pkcs8_private_keys(&mut key_reader)?
+  } else {
+    panic!("Unknown key format")
+  };
   assert_eq!(keys.len(), 1);
   let key = rustls::PrivateKey(keys.remove(0));
 
@@ -23,6 +32,7 @@ pub fn read_certs_from_file() -> Result<(Vec<rustls::Certificate>, rustls::Priva
 }
 
 #[test]
+#[cfg(feature = "dev")]
 fn gen() -> Result<()> {
   use rcgen::generate_simple_self_signed;
   let subject_alt_names = vec!["hello.world.example".to_string(), "localhost".to_string()];
