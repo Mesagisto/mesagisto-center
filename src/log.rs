0c66f965fd8038b66da067dc1d630f8597f436cf
Itsusinn <itsusinn@meowcat.org>
2022-09-19 14:43:42 +0800

    feat: support tokio-console

--- src/log.rs
+++ src/log.rs
@@ -5,28 +5,34 @@ use tracing_error::ErrorLayer;
 use tracing_subscriber::{layer::SubscriberExt, prelude::*};
 
 pub(crate) async fn init() -> Result<()> {
-  let filter = tracing_subscriber::filter::Targets::new()
+  let mut filter = tracing_subscriber::filter::Targets::new()
     .with_target("mesagisto_center", Level::TRACE)
+    .with_target("quinn", Level::INFO)
+    .with_target("tokio-tungstenite", Level::INFO)
     .with_default(Level::WARN);
 
+  if cfg!(feature = "tokio-console") {
+    filter = filter
+      .with_target("tokio", Level::TRACE)
+      .with_target("runtime", Level::TRACE);
+  }
   let registry = tracing_subscriber::registry();
 
-  registry
-    .with(filter)
-    .with(ErrorLayer::default())
-    .with(
-      tracing_subscriber::fmt::layer()
-        .with_target(true)
-        .with_timer(tracing_subscriber::fmt::time::OffsetTime::new(
-          time::UtcOffset::from_whole_seconds(
-            Local.timestamp(0, 0).offset().fix().local_minus_utc(),
-          )
+  let registry = registry.with(filter).with(ErrorLayer::default()).with(
+    tracing_subscriber::fmt::layer()
+      .with_target(true)
+      .with_timer(tracing_subscriber::fmt::time::OffsetTime::new(
+        time::UtcOffset::from_whole_seconds(Local.timestamp(0, 0).offset().fix().local_minus_utc())
           .unwrap_or(time::UtcOffset::UTC),
-          time::macros::format_description!(
-            "[year repr:last_two]-[month]-[day] [hour]:[minute]:[second]"
-          ),
-        )),
-    )
-    .try_init()?;
+        time::macros::format_description!(
+          "[year repr:last_two]-[month]-[day] [hour]:[minute]:[second]"
+        ),
+      )),
+  );
+
+  #[cfg(feature = "tokio-console")]
+  registry.with(console_subscriber::spawn()).init();
+  #[cfg(not(feature = "tokio-console"))]
+  registry.init();
   Ok(())
 }
